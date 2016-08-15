b7530c3f5bf605211207be532dcb7a1620ea196a
Jesus Velasquez <jesusmvs10@gmail.com>
2016-08-15 00:27:38 -0400

    Se empezo desde 0, se agrego una barra lateral para dar mayor vistosidad a la pagina, se afinaron los detalles responsivos para que toda la cabecera se vea correctamente y el layout de la cabecera

--- footer.php
+++ footer.php
@@ -4,10 +4,14 @@
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
      <script type="text/javascript">
-      $(".button-collapse").sideNav();
+      
 	   $(document).ready(function(){
         $('.modal-trigger').leanModal();
       });
+
+    $(".button-collapse").sideNav();
+     //$('.collapsible').collapsible();
+     //$('.button-collapse').sideNav('show');
   </script>
    
   </body>
--- header.php
+++ header.php
@@ -3,6 +3,9 @@
   <head>
     <!--Import Google Icon Font-->
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Denk+One' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Dosis:600,700' rel='stylesheet' type='text/css'>
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
     <link rel="stylesheet" type="text/css" href="css/estilo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -11,38 +14,43 @@
   </head>
 
   <body>
-    <nav>
-      <div class="nav-wrapper">
-        <a href="index.php" class="brand-logo">Portafolio</a>
-
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li id="inicio" class=""><a href="index.php"><i class="material-icons left">perm_identity</i>Inicio</a></li>
-          <li id="conocimientos"><a class="dropdown-button" data-activates="dropdown1" href="#!"><i class="material-icons left">info</i>Conocimientos<i class="material-icons right">arrow_drop_down</i></a></li>
-          <ul id="dropdown1" class="dropdown-content">
-            <!--<li><a href="#!">Analisis Funcional</a></li>-->
-            <li><a href="bd.php">Bases de Datos</a></li>
-            <li><a href="desarrollo-web.php">Desarrollo Web</a></li>
-            <li><a href="cms.php">CMS</a></li>
-          </ul>
-          <ul id="dropdown2" class="dropdown-content">
-            <!--<li><a href="#!">Analisis Funcional</a></li>-->
-            <li><a href="bd.php">Bases de Datos</a></li>
-            <li><a href="desarrollo-web.php">Desarrollo Web</a></li>
-            <li><a href="cms.php">CMS</a></li>
-          </ul>
-          <li><a href="proyectos.php"><i class="material-icons left">work</i>Proyectos</a></li>
-        </ul>
-        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-        <ul class="side-nav" id="mobile-demo">
-          <li class="">
-          <a href="index.php"><i class="material-icons left">perm_identity</i>Inicio</a>
-          <a class="dropdown-button" data-activates="dropdown2" href="#!"><i class="material-icons left">info</i>Conocimientos<i class="material-icons right">arrow_drop_down</i></a>
-          <a href="proyectos.php"><i class="material-icons left">work</i>Proyectos</a>
-          </li>
-          
-        </ul>
-      </div>
-    </nav>
-    <div class="container z-depth-2">
+    <header>
+      
       <div class="row">
+        <div id="navLateral" class="col s12 m3">
+          <ul id="slide-out" class="side-nav fixed">
+              <li><div class="userView">
+                <img class="background" src="">
+                <a href="#!user"><img class="circle" src=""></a>
+                <a href="#!name"><span class="white-text name">John Doe</span></a>
+                <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+              </div></li>
+              <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+              <li><a href="#!">Second Link</a></li>
+              <li><div class="divider"></div></li>
+              <li><a class="subheader">Subheader</a></li>
+              <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+            </ul>
+          </div>
+
+          <div id="contPrincipal" class="col s12 m9">
+            <nav class="no-sombra nav-grande block">
+         
+              <div class="row">
+                <div class="col s2 m2 valign-wrapper down">
+                  <a href="#" data-activates="slide-out" class="button-collapse"><i id="icono-menu" class="material-icons">menu</i></a></div>
+                <div class="col s8 m8 center">
+                  <h1><a class="fuente" href="#">Portafolio</a></h1>
+                
+                </div>
+              </div>
+            </nav>
+          </div>      
+        </div>
+        
+      
+       
+      </header>
+
+
       
